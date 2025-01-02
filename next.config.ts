a5655e0ba6f603768c88595b77f6377fc9ccfcf0
marrrkkk <definitelynotmark13@gmail.com>
2025-01-02 13:16:17 +0800

    chore: update next.config.ts to allow images from clerk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**", // Allow all paths under this domain
+      },
+    ],
+  },
 };
 
 export default nextConfig;
